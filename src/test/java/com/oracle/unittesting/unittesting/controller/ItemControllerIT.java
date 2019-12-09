56c594ca4377370955c6c65865baa4981edc8cef
JO20072605 <joao.romao@wipro.com>
2019-12-09 17:11:24 +0000

    implementing Integration Test underway

---
+++ src/test/java/com/oracle/unittesting/unittesting/controller/ItemControllerIT.java
@@ -0,0 +1,23 @@
+package com.oracle.unittesting.unittesting.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemControllerIT {
+
+    // integration test
+    // the SpringBootTest annotation goes and searches for a class with the @SpringBootApplication annotation
+    // wen its finds the class, it boots the application
+
+
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
